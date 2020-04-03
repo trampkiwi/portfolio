30357c003d3786d9af45dd02b93e2be9faa64175
Youngjun Lee <starwatcherkiwi@gmail.com>
2020-04-03 18:16:40 +0100

    Finishing up about.html.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $(function(){
 
     id = parseInt(id);
 
-    $.getJSON('dialogs.json?v=26', function(data){
+    $.getJSON('dialogs.json?v=27', function(data){
         var dialogs = data.dialogs;
 
         $('#content').html('');
